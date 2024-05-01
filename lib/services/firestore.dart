88f45c810e7a30584ebc32b255eb6a5d8c5fc508
LemuDev <lemu200011@gmail.com>
2024-04-30 21:24:31 -0300

    create and read

---
+++ lib/services/firestore.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  // get collection of notes
+  final CollectionReference notes =
+      FirebaseFirestore.instance.collection('notes');
+  // CREATE: add a new note
+  Future<void> addNote(String note) {
+    return notes.add({
+      'note': note,
+      'timestamp': Timestamp.now(),
+    });
+  }
+
+  // READ: get notes from database
+  Stream<QuerySnapshot> getNotesStream() {
+    final notesStream =
+        notes.orderBy('timestamp', descending: true).snapshots();
+
+    return notesStream;
+  }
+
+  // UPDATE: update notes given a doc id
+
+  // DELETE: delete notes given a doc id
+}
